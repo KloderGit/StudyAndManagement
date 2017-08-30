8e2790863c5632af53eafc65a0f73c1a07e51cdc
Kloder <kloder3@gmail.com>
2017-08-30 19:17:22 +0300

    Добавил разделяемый общий слой Common Добавил пакет Automapper

---
+++ SaM.Common/AutoMapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SaM.BusinessLogic.POCO;
+using SaM.Domain.Core.Education;
+
+namespace SaM.Common.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category, CategoryPOCO>();
+        }
+    }
+}
