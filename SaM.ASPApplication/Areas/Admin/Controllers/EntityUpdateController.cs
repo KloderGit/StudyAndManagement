f0639eeb197351b3755e3c5d2870783c14a7c17d
Kloder <kloder3@gmail.com>
2017-09-01 16:09:54 +0300

    add - Контроллер UpdateEntity

---
+++ SaM.ASPApplication/Areas/Admin/Controllers/EntityUpdateController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SaM.ASPApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class EntityUpdateController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var ddd = "AAAAAAAAAAAAAAAAAAA";
+            return View((object)ddd);
+        }
+    }
+}
